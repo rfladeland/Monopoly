988d37066a5c4fda6f1f9e5af7655b5e4a16c1ba
Jeremy Currie <jcurrie@student.neumont.edu>
2016-11-15 13:55:56 -0700

    Updated cards

--- src/Monopoly/cards.java
+++ src/Monopoly/cards.java
@@ -3,9 +3,14 @@ package Monopoly;
 import java.util.ArrayList;
 
 public class cards {
+	private ArrayList<String> CommunityChest = new ArrayList<>();
+	private ArrayList<String> Chance = new ArrayList<>();
 	
+	public void main(String[] args) {
+		getcards();
+	}
 	private  cards(){
-		ArrayList<String> Chance = new ArrayList<>();
+		
 		Chance.add("Advance to Go (Collect $200)");
 		Chance.add("Advance to Illinois Ave. - If you pass Go, collect $200.");
 		Chance.add("Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.");
@@ -22,7 +27,8 @@ public class cards {
 		Chance.add("You have been elected Chairman of the Board – Pay each player $50");
 		Chance.add("Your building loan matures – Collect $150 ");
 		Chance.add("You have won a crossword competition - Collect $100");		
-		ArrayList<String> CommunityChest = new ArrayList<>();
+		
+		
 		CommunityChest.add("Advance to Go (Collect $200)");
 		CommunityChest.add("Bank error in your favor – Collect $200");
 		CommunityChest.add("Doctor's fees– Pay $50");
@@ -44,7 +50,10 @@ public class cards {
 
 	}
 	public void getcards(){
-		
+	
+		double communitychestindex = (Math.random()*CommunityChest.size());
+		String communitychestcard = CommunityChest.get((int) communitychestindex);
+		System.out.println(communitychestcard);
 	}
 	
 }

72ac56e3a0d0f602fc5dd5c2ec309fc1c41cfebc
Christian Griffus <CGriffus@student.neumont.edu>
2016-11-16 13:08:19 -0700

    Finished properties, added deeds class

---
+++ src/Monopoly/Deeds.java
@@ -0,0 +1,29 @@
+package Monopoly;
+
+public class Deeds {
+
+	public int Houses() {
+		
+		
+		return 0;
+	}
+	
+	public int Hotels() {
+		
+		
+		return 0;
+	}
+	
+	public boolean amITaken() {
+		
+		
+		return false;
+	}
+	
+	public boolean Take() {
+		
+		
+		return false;
+	}
+	
+}

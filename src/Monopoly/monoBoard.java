c0dd6c3cc626863d272aaaf66a7028845c4968a3
RussellFladeland <RFladeland@student.neumont.edu>
2016-11-17 13:25:50 -0700

    edited board

--- src/Monopoly/monoBoard.java
+++ src/Monopoly/monoBoard.java
@@ -2,4 +2,30 @@ package Monopoly;
 
 public class monoBoard {
 	
+//	[go][02][03][04][05][06][07][08][09][10][11](jail / passing jail)
+//	[40]                              		[12]
+//	[39]				      				[13]
+//	[38]	   			      				[14]
+//	[37]				      				[15]
+//	[36]	   [Jail]  	      				[16]
+//	[35]		   		   		 			[17]
+//	[34]				   		  			[18]
+//	[33]				     				[19]
+//	[32]				     				[20]
+//	[31][30][29][28][27][26][25][24][23][22][21]
+//	go to				  					free
+//	jail				  					parking
+	public monoBoard(){
+		
+	}
+	
+	public void moveBoard(Piece player,int curSpace, int moveSpaces){
+		
+	}
+	
+	public void goToJail(Piece player){
+		
+	}
+	
+	
 }

994f4f875de49d87ee36ffc1cde4dba8f1b90cdd
Christian Griffus <CGriffus@student.neumont.edu>
2016-11-17 13:28:29 -0700

    Added methods to deeds class

--- src/Monopoly/Deeds.java
+++ src/Monopoly/Deeds.java
@@ -1,29 +1,50 @@
 package Monopoly;
 
+import java.io.IOException;
+
+import ConsoleUI.ConsoleUI;
+
 public class Deeds {
 
-	public int Houses() {
-		
-		
-		return 0;
+	private int numOfHouses;
+	private int numOfHotels;
+	private int purchasePrice;
+	private int houseHotelPurchasePrice;
+	private int rent;
+
+	public int purchaseHouses() throws IOException {
+
+		numOfHouses = ConsoleUI.promptForInt("How many houses would you like to purchase?", 0, 4);
+
+		purchasePrice = houseHotelPurchasePrice * numOfHouses;
+
+		System.out.println(
+				"You have purchased " + numOfHouses + " houses, and the total cost was " + purchasePrice + " dollars.");
+
+		return purchasePrice;
 	}
-	
-	public int Hotels() {
-		
-		
-		return 0;
+
+	public int purchaseHotels() throws IOException {
+		numOfHotels = ConsoleUI.promptForInt("How many hotels would you like to purchase?", 0, 1);
+
+		purchasePrice = houseHotelPurchasePrice * numOfHotels;
+
+		System.out.println(
+				"You have purchased " + numOfHotels + " hotels, and the total cost was " + purchasePrice + " dollars.");
+
+		return purchasePrice;
 	}
-	
+
 	public boolean amITaken() {
-		
-		
+
+		return false;
+	}
+
+	public boolean take() {
+
 		return false;
 	}
 	
-	public boolean Take() {
-		
-		
-		return false;
-	}
 	
+
 }
--- src/Monopoly/Properties.java
+++ src/Monopoly/Properties.java
@@ -3,58 +3,97 @@ package Monopoly;
 import java.util.ArrayList;
 
 public class Properties {
-	
-	public void properties() {
-	
-	ArrayList<String> brown = new ArrayList<String>();
-	ArrayList<String> lightBlue = new ArrayList<String>();
-	ArrayList<String> pink = new ArrayList<String>();
-	ArrayList<String> orange = new ArrayList<String>();
-	ArrayList<String> red = new ArrayList<String>();
-	ArrayList<String> yellow = new ArrayList<String>();
-	ArrayList<String> green = new ArrayList<String>();
-	ArrayList<String> darkBlue = new ArrayList<String>();
-	ArrayList<String> railroad = new ArrayList<String>();
-	ArrayList<String> utilities = new ArrayList<String>();
-	
-	brown.add("Mediterranean Avenue");
-	brown.add("Baltic Avenue");
-	
-	lightBlue.add("Oriental Avenue");
-	lightBlue.add("Vermont Avenue");
-	lightBlue.add("Connecticut Avenue");
-	
-	pink.add("St. Charles Place");
-	pink.add("States Avenue");
-	pink.add("Virginia Avenue");
-	
-	orange.add("St. James Place");
-	orange.add("Tennessee Avenue");
-	orange.add("New York Avenue");
-	
-	red.add("Kentucky Avenue");
-	red.add("Indiana Avenue");
-	red.add("Illinios Avenue");
-	
-	yellow.add("Atlantic Avenue");
-	yellow.add("Ventnor Avenue");
-	yellow.add("Marvin Gardens");
-	
-	green.add("Pacific Avenue");
-	green.add("North Carolina Avenue");
-	green.add("Pennsylvania Avenue");
-	
-	darkBlue.add("Park Place");
-	darkBlue.add("Boardwalk");
-	
-	railroad.add("Reading Railroad");
-	railroad.add("Pennsylvania Railroad");
-	railroad.add("B.& O. Railroad");
-	railroad.add("Short Line");
-	
-	utilities.add("Electric Company");
-	utilities.add("Water Works");
+
+	private int houseHotelPurchasePrice;
+	private String color = "";
+	private Deeds deed = null;
+
+	public void listOfProperties() {
+
+		ArrayList<String> brown = new ArrayList<String>();
+		ArrayList<String> lightBlue = new ArrayList<String>();
+		ArrayList<String> pink = new ArrayList<String>();
+		ArrayList<String> orange = new ArrayList<String>();
+		ArrayList<String> red = new ArrayList<String>();
+		ArrayList<String> yellow = new ArrayList<String>();
+		ArrayList<String> green = new ArrayList<String>();
+		ArrayList<String> darkBlue = new ArrayList<String>();
+		ArrayList<String> railroad = new ArrayList<String>();
+		ArrayList<String> utilities = new ArrayList<String>();
+
+		brown.add("Mediterranean Avenue");
+		brown.add("Baltic Avenue");
+
+		lightBlue.add("Oriental Avenue");
+		lightBlue.add("Vermont Avenue");
+		lightBlue.add("Connecticut Avenue");
+
+		pink.add("St. Charles Place");
+		pink.add("States Avenue");
+		pink.add("Virginia Avenue");
+
+		orange.add("St. James Place");
+		orange.add("Tennessee Avenue");
+		orange.add("New York Avenue");
+
+		red.add("Kentucky Avenue");
+		red.add("Indiana Avenue");
+		red.add("Illinios Avenue");
+
+		yellow.add("Atlantic Avenue");
+		yellow.add("Ventnor Avenue");
+		yellow.add("Marvin Gardens");
+
+		green.add("Pacific Avenue");
+		green.add("North Carolina Avenue");
+		green.add("Pennsylvania Avenue");
+
+		darkBlue.add("Park Place");
+		darkBlue.add("Boardwalk");
+
+		railroad.add("Reading Railroad");
+		railroad.add("Pennsylvania Railroad");
+		railroad.add("B.& O. Railroad");
+		railroad.add("Short Line");
+
+		utilities.add("Electric Company");
+		utilities.add("Water Works");
 	}
-	
-	
+
+	public int brownAndLightBlueHousePrice() {
+
+		houseHotelPurchasePrice = 50;
+
+		return houseHotelPurchasePrice;
+	}
+
+	public int orangeAndPinkHousePrice() {
+
+		houseHotelPurchasePrice = 100;
+
+		return houseHotelPurchasePrice;
+	}
+
+	public int redAndYellowHousePrice() {
+
+		houseHotelPurchasePrice = 150;
+
+		return houseHotelPurchasePrice;
+	}
+
+	public int greenAndDarkBlueHousePrice() {
+
+		houseHotelPurchasePrice = 200;
+
+		return houseHotelPurchasePrice;
+	}
+
+	public int getHouseHotelPurchasePrice() {
+		return houseHotelPurchasePrice;
+	}
+
+	public void setHouseHotelPurchasePrice(int housePurchasePrice) {
+		this.houseHotelPurchasePrice = housePurchasePrice;
+	}
+
 }

7042dc07695725ce4d5c6e5f7802413c332795eb
Tien Cu <TCu@student.neumont.edu>
2016-11-21 14:12:39 -0700

    moved some code to runner

--- src/Monopoly/monopolyRunner.java
+++ src/Monopoly/monopolyRunner.java
@@ -8,6 +8,20 @@ public class monopolyRunner {
 	private static monoMenu mm = new monoMenu();
 	public static void main(String[] args) throws IOException {
 		System.out.println("Monopoly is booted up.");
+		final int MAX_PLAYERS = 8;
+		final int MIN_PLAYERS = 2;
+		boolean playerInJail = false;
+		int jailTurnCounter = 0;
+		int location = 1;
+		int playerMoney = 1300;
+		int playerMoneyOwed = 0;
+		String[] playerStartMenu = { "Roll Dice", "View Stats" };
+		// input players amount
+		int numberOfPlayers = ConsoleUI.promptForInt("How many people will be playing?", MIN_PLAYERS, MAX_PLAYERS);
+		// ask each players entered what game piece they want
+		System.out.println("There are " + numberOfPlayers + " players for this game.");
+		int i = 0;
+		gamePieces playerPiece = null;
 		
 		mm.menu();
 //		cards card = new cards();
